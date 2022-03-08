04a989836f2e5576d2832f7e40b8e7949ccd2dcf
XII <jacopo.cartabmb@gmail.com>
2022-03-08 11:25:24 +0100

    commentato tutto il codice del file

--- server.js
+++ server.js
@@ -1,101 +1,144 @@
-const express = require("express");
+//dichiarazione librerie usate
+const express = require("express");  //server 
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const fs = require("fs");
-const moment = require("moment");
+const fs = require("fs");	//file stream lib
+const moment = require("moment");	//date formatter
 
-//load static folder for static element
+//caricamento /static folder per express
 app.use("/static", express.static("./static"));
 
+//caricamento della pagina iniziale quando una persona apre la pagina web
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
+//lettura del file json data.json (dove sono presenti tutti i dati dei posti del cinema) 
+//questa variabile verrà usata ogni volta che si vuole modificare/leggere i dati di un sedile
 let data = JSON.parse(fs.readFileSync(__dirname + "/static/data/data.json"));
 
+//controllo iniziale non appena si avvia il server, ossia controllo di integrità 
+//per verificare che se non esiste nessun proprietario del posto (quindi lo spazio ownedBy è vuoto) 
+//in automatico lo status diventa 0 e viene ripristinata elem.timeStamp
 data.posti.forEach(arr => arr.forEach(elem => {
-	if(elem.ownedBy == "") elem.statusSeat = 0;
-	fs.writeFileSync(__dirname + "/static/data/data.json", JSON.stringify(data, null, "\t"));
+	//TODO -> verificare per ogni campo se viene rilevato un qualche errore
+	if(elem.ownedBy == "" && elem.statusSeat != 0 && elem.timeStamp != "") {
+		elem.statusSeat = 0;
+		elem.timeStamp = "";
+		//sovrascrittura del file ogni qualvolta si trova l'errore
+		fs.writeFileSync(__dirname + "/static/data/data.json", JSON.stringify(data, null, "\t"));
+	}
 }));
 
-//quando un client effettua una connessione
+//quando un client effettua la connessione alla pagina
 io.on("connection", async(socket) => {
 
-	//read user log file to see if socket ip already exist = already visited the site
+	//lettura del file user_data contenente tutti i dati delle varie persone che entrano
 	let user_data = JSON.parse(fs.readFileSync(__dirname + "/static/data/user_log.json", "utf-8"));
 
+	//il socket si mette in ascolto dell'evento "ip_info_to_server" ossia aspetta che il server gli mandi la richiesta dell'evento e in caso prende gli arg che il server gli ha passato
 	socket.on("ip_info_to_server", (arg) => {
 	
+		//creazione array con tutti gli ip degli utenti che accedono al sito
 		let ip_array = user_data.map(data => {return data.ip_info.ip;});
 		
+		//controllo per verificare se la persona che entra è mai entrata nella pagina o meno
+		//se l'array di ip (ip_array) non include l'ip passato negli argument allora si tratta di un nuovo utente connesso altrimenti no
 		if (!ip_array.includes(arg.ip_info.ip)) {
-			//if a new user sava all the data (ip and socket id) in the json
 			console.log("new user connected -> ");
 
+			//user_data (il file contenente tutti i dati salvati degli utenti) è un array quindi si possono applicare
+			//tutte le funzioni base di JS (in questo caso viene usato il metodo push che aggiunge alla fine dell'array gli args che sono stati passati)
+			//gli args passati sono l'ip + informazioni base sulla connessione, socket id e nickname
 			user_data.push(arg);
+
 			fs.writeFileSync(__dirname + "/static/data/user_log.json", JSON.stringify(user_data, null, "\t"));
 		} else {
 			
+			//essendo che lo user esiste già si usa la funzione find per trovare lo slot esatto dell'array
+			//in questo caso si effettua la ricerca con gli ip quindi si controlla se l'ip dell'elemento che sta scorrendo è uguale a quello passato negli args
+			//il tutto viene salvato nella variabile "user" che ci servirà ogni qualvolta vogliamo modificare qualcosa nei dati sadella connessione trovato
 			let user = user_data.find(element => element.ip_info.ip == arg.ip_info.ip);
 
-			//if the ip already entered the site one time, modify only the socket id
 			console.log("existing user connected ->", {ip: arg.ip_info.ip, socket_id: arg.socket_id, username: arg.username});
 
-			//finding the exact slot inside the json (retrieving json)
-			
+			//controllo per vedere se la connessione (quindi ip) ha usato un nuovo nickname rispetto a quello salvato in precedenza (in questo modo si ha una visione completa dei nickname degli utenti etc)
+			//se l'array di username della variabile user non include lo username passato allora tramite il metodo push si aggiunge all'array degli username di user
 			if(!user.username.includes(arg.username[0])) user.username.push(arg.username[0]);
+			//se il socket id passato è != a unavailable allora viene sovrascritto il campo socket_id dello user
 			if(arg.socket_id != "unavailable") user.socket_id = arg.socket_id;
 			
-			//re-writing the file with the edited user_data
+			//sovrascrittura file
 			fs.writeFileSync(__dirname + "/static/data/user_log.json", JSON.stringify(user_data, null, "\t"));
 		}
 
 		let isAdmin;
+
+		//controllo sullo username, se è admin:admin (quindi l'admin della pagina | diverse funzionalità rispetto allo user normale)
+		//allora mette il boolean isAdmin a true altrimenti a false
 		arg.username == "admin:admin" ? isAdmin = true : isAdmin = false; 
 
+		//invio al socket l'evento functionForClient con request di init_btn ossia il caricamento dei bottoni 
+		//(change nickname acquista per user) e (clear set per admin)
 		socket.emit("functionForClient", {
 			req: "init_btn",
+			//la variabile isAdmin decreterà se è necessario caricare i bottoni per lo user normale o per l'admin a seconda se true o false
 			admin: isAdmin,
 		});
 	});
 
+	//il socket si mette in ascolto dell'evento "functionForServer" quindi aspetta una qualsiasi chiamata da parte del cliente per modificare dei posti nella matrice o per il cambio di nickname dell'utente (in questo caso)
 	socket.on("functionForServer", (arg) => {
 
+		//sovrascrittura della variabile data per ottenere i valori esatti al momento della chiamata;
 		data = JSON.parse(fs.readFileSync(__dirname + "/static/data/data.json"));
 		let index, seat;
 
+		//switch con parametro arg.req (arg.req contiene in pooche parole l'azione che deve essere eseguita dal client)
 		switch (arg.req) {
 		case "updatedSeat":
 
+			//in caso di posto modificato si ottiene l'index del sedile cliccato effetuando una substring 
+			//dato che l'id che gli verrà passato è tipo n0-1 bisogna eliminare la n (appunto con la substring dove si prendono solo 0-1 e infine splittare la stringa con divisore "-")
+			//con la funzione split viene creato un array degli elementi separati dal divisore "-" quindi per richiamare l'elemento dobbiamo specificare anche l'indice 
+			//in questo caso la variabile index sarà cosi -> ["0", "1"]
 			index = arg.id.substring(1,4).split("-");
             
 			console.log(`clicked seat, row n.${parseInt(index[0]) +1}, place n.${parseInt(index[1]) + 1}`);
 
-			//prende il posto esatto della matrice del sedile cliccato tramite gli args passati (in questo caso array di indici della matrice) e conversione del nmumero del sedile con XOR -> ^=1 (cosi non ci sono controlli ulteriori ma in automatico viene switchato)
+			//prende le coordinate esatte esatte della matrice del sedile cliccato tramite gli args passati (in questo caso array di indici della matrice)
 			seat = data.posti[index[0]][index[1]];
+			//conversione del nmumero del sedile con XOR -> ^=1 (cosi non ci sono controlli ulteriori ma in automatico viene switchato da 0 a 1 e viceversa)
 			seat.statusSeat ^= 1, seat.ownedBy = arg.status.ownedBy;
+			//controllo sul timestamp passato, se esiste il timestamp viene convertito in formato "01/01/1970 - 00:00:00" altrimenti vuoto
 			seat.timeStamp = arg.status.timeStamp ? moment.unix(arg.status.timeStamp).format("DD/MM/YYYY - HH:mm:ss") : "";
 			
+			//sovrascrizione del file con i dati modificati (quindi numero dello stato del sedile e timestamp)
 			fs.writeFileSync(__dirname + "/static/data/data.json", JSON.stringify(data, null, "\t"));
+			//send a tutti i client l'evento "updatedSeatfromServer"
 			io.emit("functionForClient", { req: "updatedSeatfromServer" });
 			break;
 		case "changeNickname":
 
+			//scorro l'intera matrice dei posti e controllo se il posto.ownedBy (il nickname della persona che ha prenotato il posto) 
+			//è uguale a quello passato in arg (in questo caso arg.oldName perche bisogna cercare i nick vecchio) e in caso sostituirlo con arg.newName
+			//ossia il nuovo nickname scelto dall'utente
 			data.posti.forEach(row => row.forEach(seat => {
 				if(seat.ownedBy == arg.oldName) seat.ownedBy = arg.newName;
 			}));
 		
+			//sovrascrittura file
 			fs.writeFileSync("./static/data/data.json", JSON.stringify(data, null, "\t"));
 			io.emit("functionForClient", { req: "updatedSeatfromServer" });
-	
 			break;
 		}
 	});
 });
 
+//il server si mette in ascolto sulla porta 3000, *http://localhost:3000/*
 server.listen(3000, () => {
 	console.log("http://localhost:3000/");
 });
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-unused-vars */
+
+//dichiarazione di variabili che potranno essere usate in qualsiasi funzione
+//infatti in js se si dichiarano delle variabili con var viene modificato lo scoping 
+//quindi la variabile sarà modificabile in qualsiasi punto del file
+//con let invece viene settato lo scoping locale quindi non si può modificare la funzione fuori dalle funzioni in cui è stata dichiarata
 var el_array = new Array();
 var el_counter = 0;
 var counter_div = document.getElementById("counter");
@@ -6,13 +11,17 @@ var counter_div = document.getElementById("counter");
 async function init(socket) {
 	let txt;
 
+	//do while finchè non viene inserito un username corretto (in questo caso lo username non deve essere ne vuoto ne spazi vuoti)
 	do {
 		txt = prompt("Inserisci il tuo username");
 	}	
 	while (txt == "" || txt == null);
 
+	//gestione bug di socket.io, se viene effettuato tante volte il refresh ogni tanto il l'id del socket non viene salvaro
+	//se dovesse succedere questo l'id viene modificato in unavailable cosi da farlo capire al server
 	if (!socket.id) socket.id = "unavailable";
 
+	//funziona che tramite l'host di cloudflare prende i dati della connessione quindi ip location user agent versione ecc
 	let ip_info = await getIP();
 
 	await socket.emit("ip_info_to_server", {
@@ -26,44 +35,62 @@ async function init(socket) {
 		username: [txt],
 	});
 
-	//creazione base dei posti dal file json
+	//creazione base nel file index dei posti file json
 	initialfetchSeat(socket, txt);
 
 	let element;
 
+	//selezione dell'elemento con id username cosi da poter essere modificato quando verrà inserito il nick e inizializzazione var newNickname
 	let tagWelcome = document.getElementById("username"), newNickname;
+	//selezione dell'id sub-menu dove saranno messi tutti i bottoni quindi acquista/clear e change nickname
 	let subMenu = document.getElementsByClassName("sub-menu")[0];
 
+	//il socket si mette in ascolto per eventuali eventi dal server
 	socket.on("functionForClient", (arg) => {
 
 		switch(arg.req) {
 		case "updatedSeatfromServer":
-			console.log("ciao");
+			//in caso di updatedSeatfromServer si fa la funzione normalFetchSeat che controlla tutti i posti a sedere e modificare il src in base agli elementi trovati
 			normalFetchSeat(txt);
 			break;
 		case "init_btn":
+			//in caso di init_btn ossia di inizializzazione dei bottoni si controlla prima di tutto se l'arg passato è admin
+			//se è admin = true allora significa che l'accesso è stato effetuato dall'admin altrimenti da un semplice user
 			if(arg.admin) {
+
+				//testo di tagwelcome predefinito è ADMIN (sull 'html uscirebbe Benvenuto ADMIN (con admin in rosso))
 				tagWelcome.innerHTML = "ADMIN";
 
-		
+				//creazione del bottone clear_element ossia quello che permette di ripristinare a disponibili i sedili che clicca
 				let clear_element = document.createElement("button");
+				//all'inizio il bottone viene settato come disabled quindi non cliccabile finche non viene selezionato almeno un elemento
 				clear_element.disabled = true;
 				clear_element.id = "clear_btn";
 
+				//testo dentro il bottone
 				clear_element.innerHTML = "Clear seat";
 
+				//la funzione onclick permette di avviare una funzione quando viene cliccato un elemento / bottone
+				//in questo caso quando viene cliccato il bottone si controllano tutti gli input di el_array (quindi quello che contiene tutti gli elementi cliccati)
+				//e per ogni input cliccato si fa la change value cosi da poter modificarne il valore
 				clear_element.onclick = () => {
 					el_array.forEach(input => {
 						changeValue(input, socket, txt);
 					});
 				};
+				//append del bottone clear_element all'elemento sub-menu
 				subMenu.append(clear_element);
 			} else {
+				//in caso di user normale il testo mostrerà il nickname dell'utente (-> Benvenuto pincopallino)
 				tagWelcome.innerHTML = txt.toLowerCase();
 
+				//simile ad admin vengono creati i vari bottoni quindi change nickname e acquista seat
 				let nick_element = document.createElement("button");
 				nick_element.innerHTML = "Change Nickname";
 		
+				//quando viene cliccato il bottone change nickname
+				//viene chiesto il nuovo nickname e si manda al server la richiesta di changeNickname
+				//con parametri il vecchio username (che verrà controllato) e quello nuovo che sostituirà
 				nick_element.onclick = () => {
 					newNickname = prompt("Scegli il tuo nuovo nickname");
 					socket.emit("functionForServer", {
@@ -71,50 +98,76 @@ async function init(socket) {
 						oldName: txt,
 						newName: newNickname
 					});
+					//refresh forzato della pagina per poter permettere le modifiche senza errori
+					window.location.reload();
 				};
 		
 				let reserve_element = document.createElement("button");
+				//anche per acquista il bottone all'inizio è disattivato finche non vieen cliccato almeno un elemento
 				reserve_element.disabled = true;
 				reserve_element.id = "reserve_btn";
 		
 				reserve_element.innerHTML = "Acquista";
 		
+				//funzione identica a quella di clear seat poiche il funzionamento è lo stesso e il controllo verrà effetuato dentro la funzione
 				reserve_element.onclick = () => {
 					el_array.forEach(input => {
 						changeValue(input, socket, txt);
 					});
 				};
 		
+				//append dei due elementi al sub-menu
 				subMenu.append(nick_element, reserve_element);
 			}
 			break;
 		}
 	});
 
+	//add event listener è un metodo che permette tramite degli eventi (in questo caso quello del click dell'utente) di fare determinate azioni
 	document.addEventListener("click", (event) => {
 
+		//in questo caso noi vogliamo che ad ogni click del sedile il colore di esso cambi + aumenti il contatore dei posti selezionati
+
+		//dato che l'event  listener tiene conto di tutti i click bisogna prima di tutto escludere i click degli elementi che non iniziano per n
+		//con questo controllo verranno presi di considerazione solamente i seat (perche iniziano tutti con n)
 		if(!event.target.id.startsWith("n")) return;
 		
+		//serie di regex per controllare i nomi dei file
+		//regex molto semplici che controllano solamente se è presente quella determinata condizione (quindi nome del file)
+		//uso del regex poiche la path generale cambia in base al dispositivo su cui si fanno i click
+		//infatti da local l'indirizzo sarà localhost:3000 .... mentre da un altro pc sarà diverso
+		//con il regex viene quindi preso in considerazione solamente il nome del file
 		const regex_selected = (/(static\/img\/cinema_seat_selected.svg)/g);
 		const regex_oos = (/(static\/img\/cinema_seat_oos.svg)/g);
 		const regex_selectedByMe = (/(static\/img\/cinema_seat_takedByMe.svg)/g);
 
+		//se lo username è admin:admin si va ad agir
 		if(txt == "admin:admin") {
 
+			//se il contatore dei sedili selezionati è >= 0 allora si mette che il bottone clear_btn è visibile e cliccabile
 			if(el_counter >= 0) document.getElementById("clear_btn").disabled = false;
 
+			//si ottiene il selettore dell'elemento che è stato cliccato
+			//infatti l'eventlistener si tiene in memoria l'evento che lo ha azionato e da li si possono ottenere tutte le informazioni dell'elemento cliccato
+			//in questo caso si va a selezionare l'elemento tramite il target.id (quindi l'id dell'elemento cliccato)
 			let element = document.getElementById(event.target.id);
 
+			//se il test del regex_oos è true (quindi il posto è effettivamente stato preso da altre persone viene aumentato il contatore dei posti selezionati)
+			//e viene aggiunto al el_array l'intero elemento cliccato (oltre a questo viene anche modificata localmente l'immagine con quella del selected che ci 
+			//fa capire che abbiamo selezionato quell'elemento)
 			if(regex_oos.test(element.src) == true ){
 				element.src = "./static/img/cinema_seat_selected.svg";
 				el_array.push(element);
 				el_counter++;
 			} else if(regex_selected.test(element.src) == true) {
+				//controllo per verificare se clicchiamo un posto che già abbiamo cliccato prima (in poche parole qua si controlla se è stata fatta una deselezione)
+				//se l'elemento è stato deselezionato ovviamente viene ripristinata l'immagine a quello di oos e vengono eliminati dall'array e dal contatore l'elemento
 				element.src = "./static/img/cinema_seat_oos.svg";
 				el_array.pop(element);
 				el_counter--;
 			}
 			
+			//il testo del div counter_div sarà il numero corrente di sedili selezionati
 			counter_div.innerHTML = el_counter;
 
 		} else {
@@ -122,20 +175,28 @@ async function init(socket) {
 			
 			if(el_counter >= 0 || txt == "admin:admin") document.getElementById("reserve_btn").disabled = false;
 		
+			//si ottiene il selettore dell'elemento che è stato cliccato
+			//infatti l'eventlistener si tiene in memoria l'evento che lo ha azionato e da li si possono ottenere tutte le informazioni dell'elemento cliccato
+			//in questo caso si va a selezionare l'elemento tramite il target.id (quindi l'id dell'elemento cliccato)
 			element = document.getElementById(event.target.id);
 		
+			//se il posto è già stato selezionato da noi si va a fare come per admin quindi diminuzione del contatore e metodo pop dell'array el_array
+			//la funzione pop permette di eliminare da un array l'ultimo elemento che è stato inserito
 			if(regex_selected.test(element.src) == true) {
 				element.src = "./static/img/cinema_seat_available.svg";
 				el_array.pop(element);
 				el_counter--;
 			} else if(regex_oos.test(element.src) == true || regex_selectedByMe.test(element.src) == true) {
+				//in caso di posto non disponibile o di già prenotato da me non verrò contato il click e verrà mostrato a schermo la scritto che è giù stato prenotato/acquistato
 				return alert("Non puoi, già prenotato/acquistato");
 			} else {
+				//stesso funzionamento di prima se viene cliccato un posto libero viene selezionato da noi e i dati dell'elemento vengono aggunti all'array + aumento counter
 				element.src = "./static/img/cinema_seat_selected.svg";
 				el_array.push(element);
 				el_counter++;
 			}
 			
+			//il testo del div counter_div sarà il numero corrente di sedili selezionati
 			counter_div.innerHTML = el_counter;
 		}
 	});
@@ -143,37 +204,59 @@ async function init(socket) {
 
 async function initialfetchSeat(socket, nickname) {
 
+	//funzione che parte non appena viene controllato il nickname e quando si apre la pagina quindi caricamento iniziale di tutto
+
+
+	//lettura file data.json per mostrare tutti i posti che ci sono nella matrice dei posti
 	let posti_matrice = (await (await fetch("./static/data/data.json")).json()).posti;
 
+	//selettore tramite id "seats" dell'elemento creato nell'html cosi ogni sub-elemento che viene aggiunto verrà messo in append sempre nel div "seats"
 	let div = document.getElementById("seats");
 
+	//scorro tutte le row della matrice (file dei posti)
+	//usata la funzione map poiche permette direttamente di dichiarare un contatore (in questo caso i)
+	//rispetto al forEach che non permette ciò 
 	posti_matrice.map(async(row, i) => {
 
+		//per ogni row trovata creo un nuovo div con id identificativo e aggiungo alla lista delle classi del div la classe "seat-row" (aggiunge del semplice padding per ogni fila)
 		let element_div = document.createElement("div");
 		element_div.id = `row ${i}`;
 		element_div.classList.add("seat-row");
 
+		//appen del div della row nel div dei seats
 		div.appendChild(element_div);
 
+		//scorro tutti i posti della fila (row) usata sempre funziona map per permettere di avere il contatore incluso
 		row.map(async(seat, count) => {
 
 			let path = "";
 
+			//controllo dello status del sedile per decidere quale file caricare nella path dell'elemento
+			//se 0 significa che il posto è disponibile per essere prenotato
+			//se è 1 allora è occupato/riservato da me
 			if (seat.statusSeat == 0) {
 				path = "./static/img/cinema_seat_available.svg";
 			} else if (seat.statusSeat == 1) {
 
+				//controllo del nickname che ha prenotato il posto, se è uguale al nickname inserito precedentemente nel prompt (riga 16) allora verrà caricato il file takedByMe (quindi verrà visto verde poiche prenotato da noi)
+				//altrimenti viene caricato il file seat_oos poiche già stato prenotato ma non da noi
 				seat.ownedBy == nickname ? path = "./static/img/cinema_seat_takedByMe.svg" : path = "./static/img/cinema_seat_oos.svg"; 
 
 			}
 			
+			//creazione di un elemento input (sedile)
 			let element = document.createElement("input");
 
+			//aggiunta della classe seat che contiene le grandezze per ogni sedile e il padding di distanziamento per ogni sedile
 			element.classList.add("seat");
 			element.type = "image";
+			//src sarebbe il percorso per trovare l'immagine da far vedere
 			element.src = path;
+			//assegnazione id specifico per ogni posto a sedere tenendo conto anche della row (tramite la variabile i) in questo modo per ogni posto abbiamo un id specifico
+			//aggiunta la n all'inizio poiche non sono accettati id che iniziano per numero
 			element.id = `n${i}-${count}`;
 
+			//appen l'elemento appena creato (input) alla row specifica (variabile element_div)
 			element_div.appendChild(element);
 		});
 	});
@@ -181,6 +264,9 @@ async function initialfetchSeat(socket, nickname) {
 
 async function normalFetchSeat(nickname) {
 
+	//funzione chiamata ogni qualvolta c'è una modifica dei posti
+
+	//procedimento simile a initialfetchSeat per quanto riguarda lettura del file data 
 	let posti_matrice = (await (await fetch("./static/data/data.json")).json()).posti;
 
 	let div = document.getElementById("seats");
@@ -193,17 +279,14 @@ async function normalFetchSeat(nickname) {
 			let element = div.querySelector(id);
 			let path = "img/cinema_seat_oos.svg";
 
+			//controllo dello status del sedile
 			if (seat.statusSeat == 0) {
 				path = "./static/img/cinema_seat_available.svg";
 			} else if (seat.statusSeat == 1) {
-
-				if (seat.ownedBy == nickname) {
-					path = "./static/img/cinema_seat_takedByMe.svg";
-				} else {
-					path = "./static/img/cinema_seat_oos.svg";
-				}
+				//controllo identico all'initialfetchSeat per vedere se il nickname del sedile è uguale a quello dell'utente
+				seat.ownedBy == nickname ? path = "./static/img/cinema_seat_takedByMe.svg" : path = "./static/img/cinema_seat_oos.svg"; 
 			}
-			//editing all the seat based on the new json created
+			//dato che i posti sono già stati caricati dalla funzione initialfetchSeat non c'è bisogno di creare nuovi elementi ma solo i modificare l'src dell'elemento
 			element.src = path;
 		});
 	});
@@ -211,24 +294,33 @@ async function normalFetchSeat(nickname) {
 
 async function changeValue(element, socket, username) {
 
-	el_array = [];
-	counter_div.innerHTML = 0;
-	let index = element.id.replace("n", "").split("-");
+	//funzione ogni volta che si clicca il bottone acquista o clear row (admin only)
 
-	//get json file 
+	//pulizia dell'array el_array (il quale contiene tutti gli elementi che vengono cliccati prima di premere il bottone acquista/clear)
+	el_array = [];
+	//azzeramento contatore presente nell'html degli elementi cliccati
+	counter_div.innerHTML = 0;
+
+	//ottenimento dell'array degli indici dell'elemento cliccato
+	let index = element.id.substring(1,4).split("-");
+
 	let posti_matrice = (await (await fetch("static/data/data.json")).json()).posti;
 
-	//get the numer (oos or available) with index number
+	//ottenimento dello slot della matrice dell'element cliccato tramite gli indici degli id 
 	let num = posti_matrice[index[0]][index[1]];
 
+	//si controlla se lo username che clicca il bottone è admin:admin (in quel caso significa che è stato fatto il clear seat quindi bisogna sovrascrivere i dati per renderlo disponibile a tutti)
+	//c'è bisogno di pulire i dati all'interno di num (quindi ownedBy = "", status seat 0 che indica il posto libero ed eliminazione del timestamp)
 	if(username == "admin:admin") {
 		num.ownedBy = "", num.statusSeat = 0, num.timeStamp = null;
 	} else {
+		//se lo username non è admin:admin (quindi si parla di user normale)
+		//si mette come ownedBy lo username è come timestamp si mette il valore unix della funzione date .now / 1000 (in questo modo dal server si può effetuare la conversione corretta della data)
 		num.ownedBy = username;
 		num.timeStamp = Math.floor(Date.now() / 1000);
 	}
 	
-	//passing to the server with "UpdatedSeat" the index of the changed seat with the current status -> 0 --> available -> 1 --> oos
+	//chiamata al server con functionForServer e con richiuesta di aggiornare i posti a sedere (come altri parametri vengono passati gli id dell'elemento cliccato e lo status quindi i valori modificati)
 	socket.emit("functionForServer", { req: "updatedSeat", id: element.id, status: num});
 	
 }
